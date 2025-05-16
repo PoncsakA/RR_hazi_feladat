30b14e34bfaa880431dd178dc415650ce23fa9ea
PoncsakA <poncsak.akos@gmail.com>
2025-05-16 14:33:09 +0200

    reorganised packages

--- src/hw_nav_py/setup.py
+++ src/hw_nav_py/setup.py
@@ -1,6 +1,6 @@
 from setuptools import find_packages, setup
 
-package_name = 'bme_homework1_py'
+package_name = 'hw_nav_py'
 
 setup(
     name=package_name,
@@ -14,7 +14,7 @@ setup(
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='akos',
-    maintainer_email='akos@todo.todo',
+    maintainer_email='poncsak.akos@gmail.com',
     description='TODO: Package description',
     license='TODO: License declaration',
     tests_require=['pytest'],
